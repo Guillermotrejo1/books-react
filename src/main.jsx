21c07b4007664c007aa3f255cd3d4f24ef002554
Guillermo Trejo <maynardtrejo@yahoo.com>
2023-09-05 23:58:24 -0500

    pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { library } from 'fortawesome/fontawesome-svg-core'
+import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft } from 'fortawesome/free-solid-svg-icons'
 
 library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft)
 
